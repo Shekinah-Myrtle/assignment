7b41b3bd45cd916472ae20a71da5e95c91e24585
shekinah <shekinahmyrtle123@gmail.com>
2025-04-01 21:29:56 +0530

    Your commit message here

---
+++ A4.py
@@ -0,0 +1,59 @@
+#construct linear algebra library
+#for exponent
+class exponent:
+    def __init__(self,value):
+        self.value=value
+#scalar functions
+class scalar:
+    def __init__(self,value):
+        self.value=value
+    
+    def add(self,value_1):
+        addition=self.value+value_1.value
+        return addition
+    
+    def subtract(self,value_1):
+        subtraction=self.value-value_1.value
+        return subtraction
+
+    def multiply(self,value_1):
+        multiply=self.value*value_1.value
+        return multiply
+    
+    def power(self,exponent)
+        power= self.value**exponent.value
+        return power
+    
+    def exponent(self):
+        exponent=s
+
+    
+
+#add
+#sum
+#mult
+#pow
+#exp
+#size
+
+#vector functions
+class vector():
+
+#add
+#cosine
+#sine
+#dot
+#cross
+#size
+#matrix functions
+class matrix():
+
+#add
+#sub
+#mult
+#size
+
+#advanced functions
+#trace of a matrix
+#transpose of a matrix
+#svd of a matrix
